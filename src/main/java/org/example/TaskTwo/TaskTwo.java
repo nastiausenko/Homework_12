3f244517ac2a9fd539144e2c2094a05b630f1845
nastiausenko <Usenko.Anastasia@lll.kpi.ua>
2023-10-15 18:02:23 +0300

    add method number()

--- src/main/java/org/example/TaskTwo/TaskTwo.java
+++ src/main/java/org/example/TaskTwo/TaskTwo.java
@@ -64,6 +64,24 @@ public class TaskTwo {
         }
     }
 
+    public void number() {
+        synchronized (lock) {
+            while (current <= n) {
+                try {
+                    if (current % 3 != 0 && current % 5 != 0) {
+                        System.out.println(current);
+                        current++;
+                        lock.notifyAll();
+                    } else {
+                        lock.wait();
+                    }
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
     public static void main(String[] args) {
         int n = 15;
         TaskTwo taskTwo = new TaskTwo(n);

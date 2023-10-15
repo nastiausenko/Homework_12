2b320adaec185bf056e4594461ddd328e61adbbc
nastiausenko <Usenko.Anastasia@lll.kpi.ua>
2023-10-15 17:58:38 +0300

    add method fizz()

---
+++ src/main/java/org/example/TaskTwo/TaskTwo.java
@@ -0,0 +1,37 @@
+package org.example.TaskTwo;
+
+public class TaskTwo {
+    private int n;
+    private int current;
+    private final Object lock = new Object();
+
+    public TaskTwo(int n) {
+        this.n = n;
+        this.current = 1;
+    }
+
+    public void fizz() {
+        synchronized (lock) {
+            while (current <= n) {
+                try {
+                    if (current % 3 == 0 && current % 5 != 0) {
+                        System.out.println("fizz");
+                        current++;
+                        lock.notifyAll();
+                    } else {
+                        lock.wait();
+                    }
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+
+    public static void main(String[] args) {
+        int n = 15;
+        TaskTwo taskTwo = new TaskTwo(n);
+
+    }
+}
